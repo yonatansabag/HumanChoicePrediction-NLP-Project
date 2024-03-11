5bab566ac90a2aa30712b830b4d2c8ddb8638213
eilamshapira <eilamshapira@gmail.com>
2024-03-11 15:56:50 +0200

    Initial commit

---
+++ Project/RunningScripts/clone_and_init.sh
@@ -0,0 +1,10 @@
+# cloning your code from github:
+git clone https://github.com/eilamshapira/NLP_COURE_2024_PROJECT.git
+
+# Your main sweep:
+python final_sweep_demo.py
+
+# More runs appear in your report:
+#python sweep_1.py
+#python sweep_2.py
+#python sweep_3.py
---
+++ Project/RunningScripts/final_sweep_demo.py
@@ -0,0 +1,32 @@
+import wandb
+YOUR_WANDB_USERNAME = "eilamshapira"
+project = "NLP2024_PROJECT_ID1_ID2"
+
+command = [
+        "${ENVIRONMENT_VARIABLE}",
+        "${interpreter}",
+        "StrategyTransfer.py",
+        "${project}",
+        "${args}"
+    ]
+
+sweep_config = {
+    "name": "LSTM: SimFactor=0/4",
+    "method": "grid",
+    "metric": {
+        "goal": "maximize",
+        "name": "AUC.test.max"
+    },
+    "parameters": {
+        "ENV_HPT_mode": {"values": [False]},
+        "architecture": {"values": ["LSTM"]},
+        "seed": {"values": list(range(1, 6))},
+        "online_simulation_factor": {"values": [0, 4]},
+    },
+    "command": command
+}
+
+# Initialize a new sweep
+sweep_id = wandb.sweep(sweep=sweep_config, project=project)
+print("run this line to run your agent in a screen:")
+print(f"screen -dmS \"sweep_agent\" wandb agent {YOUR_WANDB_USERNAME}/{project}/{sweep_id}")
